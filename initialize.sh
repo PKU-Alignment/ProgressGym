47c83632dc2421b91b652e37f39bb385efb951d9
Tianyi (Alex) Qiu <qiutianyi.qty@gmail.com>
2024-10-06 09:44:35 -0700

    fix: specify classic solver for conda

--- initialize.sh
+++ initialize.sh
@@ -1,15 +1,15 @@
 export PYTHONNOUSERSITE=1
-CONDA_OVERRIDE_CUDA=12.1 conda env create -y --file ./src/config/conda-recipe.yaml -n pa38
-conda create --name pa38-lf --clone pa38
+CONDA_OVERRIDE_CUDA=12.1 conda env create -y --file ./src/config/conda-recipe.yaml -n pa38 --solver=classic
+conda create --name pa38-lf --clone pa38 --solver=classic
 conda activate pa38-lf
 conda run --no-capture-output -n pa38-lf pip3 install -r ./libs/llama_factory/requirements.txt
 conda run --no-capture-output -n pa38-lf pip3 install "transformers>=4.40.2" "triton>=2.0.0"
 conda activate pa38
-conda install yajl -y
+conda install yajl -y --solver=classic
 conda run --no-capture-output -n pa38 pip3 install -r ./src/config/requirements-pip.txt
 conda run --no-capture-output -n pa38 pip3 install flashinfer -i https://flashinfer.ai/whl/cu121/torch2.4/
 conda run --no-capture-output -n pa38 pip3 install -r ./libs/moralchoice/requirements.txt
-conda remove --force -y pyarrow
+conda remove --force -y pyarrow --solver=classic
 conda run --no-capture-output -n pa38 pip3 uninstall -y pyarrow
 conda run --no-capture-output -n pa38 pip3 install pyarrow
 conda run --no-capture-output -n pa38 pip3 install -U vllm
