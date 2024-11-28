a58dcd62a300c83d49cf7a793fd9c9c61eebe90d
Tianyi (Alex) Qiu <qiutianyi.qty@gmail.com>
2024-11-28 10:45:58 -0800

    fix: correct formatting of src.path.root

--- src/path.py
+++ src/path.py
@@ -1,3 +1,3 @@
 import os, sys
-root = "src".join(os.path.dirname(os.path.abspath(__file__)).split("src")[:-1])
+root = "src".join(os.path.dirname(os.path.abspath(__file__)).split("src")[:-1]).strip("/").strip("\\")
 print(f"Library root directory: {root}")
